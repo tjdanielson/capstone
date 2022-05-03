51546f6e555e8efa0f94538ec97a83163f5a0d64
tjdanielson <tessajdanielson@gmail.com>
2022-05-03 13:05:10 -0500

    Added delete and put endpoints for badges.

--- backend/badges/urls.py
+++ backend/badges/urls.py
@@ -3,4 +3,5 @@ from .import views
 
 urlpatterns = [
     path('', views.BadgeList.as_view()),
+    path('<int:pk>/', views.BadgeDetail.as_view())
 ]
--- backend/badges/views.py
+++ backend/badges/views.py
@@ -1,4 +1,6 @@
 from rest_framework.decorators import APIView
+
+from cleanups.serializers import CleanupSerializer
 from .models import Badge
 from .serializers import BadgeSerializer
 from django.http import Http404
@@ -17,6 +19,30 @@ class BadgeList(APIView, AllowAny):
     def post(self, request):
         serializer = BadgeSerializer(data=request.data)
         if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BadgeDetail(APIView, IsAuthenticated):
+
+    def get_object(self, pk):
+        try:
+            return Badge.objects.get(pk=pk)
+        except Badge.DoesNotExist:
+            raise Http404
+
+    def delete(self, request, pk):
+        badge = self.get_object(pk)
+        if request.user.is_staff ==1:
+            badge.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+    def put(self, request, pk):
+        badge = self.get_object(pk)
+        serializer = BadgeSerializer(badge, data=request.data)
+        if serializer.is_valid() and request.user.is_staff == 1:
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
