acacf4cd216be513c7e7adc9546161c50e8cedb0
tjdanielson <tessajdanielson@gmail.com>
2022-05-04 15:50:33 -0500

    Added return statement in usrStats component that displays the count of cleanups and badges. Goal tracking and user stats complete.

--- frontend/src/components/UserStats/UserStats.jsx
+++ frontend/src/components/UserStats/UserStats.jsx
@@ -1,7 +1,18 @@
 import react from "react";
 
 const UserStats = (props) => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <p>{props.cleanupCount}</p>
+        <p>Total Cleanups</p>
+      </div>
+      <div>
+        <p>{props.badgeCount}</p>
+        <p>Total Badges</p>
+      </div>
+    </div>
+  );
 };
 
 export default UserStats;
