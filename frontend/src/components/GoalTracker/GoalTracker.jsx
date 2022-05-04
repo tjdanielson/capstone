d1ea83d664cc9bbe99c5f6599638be9ab2e7c437
tjdanielson <tessajdanielson@gmail.com>
2022-05-04 13:28:50 -0500

    Added GoalTracker component and axios call to get userstats.

---
+++ frontend/src/components/GoalTracker/GoalTracker.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import useAuth from "../../hooks/useAuth";
+
+import axios from "axios";
+
+const GoalTracker = (props) => {
+  return (
+    <div>
+      <p>I'm the goal tracker</p>
+    </div>
+  );
+};
+
+export default GoalTracker;
