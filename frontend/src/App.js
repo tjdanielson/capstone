c149e83f70f8da3fc5b1b84db19f8d6b48dd0c17
tjdanielson <tessajdanielson@gmail.com>
2022-05-05 10:16:28 -0500

    Added button to show list of cleanups when clicked

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,7 +39,6 @@ function App() {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
-      <Footer />
     </div>
   );
 }
--- frontend/src/components/CleanupList/CleanupList.jsx
+++ frontend/src/components/CleanupList/CleanupList.jsx
@@ -1,31 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const CleanupList = (props) => {
-  console.log("cleanups in list:", props.cleanups);
+  const [showTable, setShowTable] = useState(false);
+
+  const handleShow = () => setShowTable(true);
+
   return (
     <div>
-      <table className="my-table">
-        <thead>
-          <tr>
-            <th>Date Logged</th>
-            <th>Date of Cleanup</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody className="table-body">
-          {props.cleanups.map((cleanup, i) => {
-            return (
-              <tr key={i}>
-                <td>{cleanup.date_submitted}</td>
-                <td>{cleanup.date_cleanup}</td>
-                {/* <td><DeleteSong songIdproperty={song.id} reloadMusic={makeGetRequest}/></td>
+      <div>
+        <>
+          <button
+            className="modal-button"
+            variant="primary"
+            onClick={handleShow}
+          >
+            See all Cleanups
+          </button>
+        </>
+      </div>
+      {showTable && (
+        <table className="my-table">
+          <thead>
+            <tr>
+              <th>Date Logged</th>
+              <th>Date of Cleanup</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody className="table-body">
+            {props.cleanups.map((cleanup, i) => {
+              return (
+                <tr key={i}>
+                  <td>{cleanup.date_submitted}</td>
+                  <td>{cleanup.date_cleanup}</td>
+                  {/* <td><DeleteSong songIdproperty={song.id} reloadMusic={makeGetRequest}/></td>
                                 <td><UpdateSong song={song} reloadMusic={makeGetRequest}/></td> */}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
