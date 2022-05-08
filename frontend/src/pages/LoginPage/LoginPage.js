56ce87eccd5130829fb3d962497ff1436bea5489
tjdanielson <tessajdanielson@gmail.com>
2022-05-08 15:34:52 -0500

    Added styling to the login page.

--- frontend/src/pages/LoginPage/LoginPage.js
+++ frontend/src/pages/LoginPage/LoginPage.js
@@ -2,7 +2,9 @@ import React, { useContext, useEffect } from "react";
 import AuthContext from "../../context/AuthContext";
 import useCustomForm from "../../hooks/useCustomForm";
 import { Link } from "react-router-dom";
+import "../../App.css";
 import "./LoginPage.css";
+import bootstrap from "bootstrap";
 
 const LoginPage = () => {
   const { loginUser, isServerError } = useContext(AuthContext);
@@ -19,32 +21,48 @@ const LoginPage = () => {
   }, [isServerError]);
 
   return (
-    <div className="container">
-      <form className="form" onSubmit={handleSubmit}>
-        <label>
-          Username:{" "}
-          <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleInputChange}
-          />
-        </label>
-        <label>
-          Password:{" "}
-          <input
-            type="text"
-            name="password"
-            value={formData.password}
-            onChange={handleInputChange}
-          />
-        </label>
-        {isServerError ? (
-          <p className="error">Login failed, incorrect credentials!</p>
-        ) : null}
-        <Link to="/register">Click to register!</Link>
-        <button>Login!</button>
-      </form>
+    <div className="login-page">
+      <div className="container">
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form-group row">
+            <label className="col-sm-2 col-form-label">
+              Username{" "}
+              <div className="col-sm-10">
+                <input
+                  className="input"
+                  type="text"
+                  name="username"
+                  value={formData.username}
+                  onChange={handleInputChange}
+                />
+              </div>
+            </label>
+          </div>
+          <div className="form-group row">
+            <label className="col-sm-2 col-form-label">
+              Password{" "}
+              <div className="col-sm-10">
+                <input
+                  className="input"
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                />
+              </div>
+            </label>
+          </div>
+          {isServerError ? (
+            <p className="error">Login failed, incorrect credentials!</p>
+          ) : null}
+          <div className="form-group row">
+            <Link to="/register">Click to register!</Link>
+            <div className="col-sm-10">
+              <button className="login-button">Login!</button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
