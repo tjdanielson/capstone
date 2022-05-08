666cf5828e29b5f0c11ff65031de32601f19d526
tjdanielson <tessajdanielson@gmail.com>
2022-05-08 17:40:59 -0500

    Added styling to registration page.

--- frontend/src/pages/RegisterPage/RegisterPage.js
+++ frontend/src/pages/RegisterPage/RegisterPage.js
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import AuthContext from "../../context/AuthContext";
 import useCustomForm from "../../hooks/useCustomForm";
+import "../../App.css";
+import "./RegisterPage.css";
 
 const RegisterPage = () => {
   const { registerUser } = useContext(AuthContext);
@@ -17,59 +19,88 @@ const RegisterPage = () => {
   );
 
   return (
-    <div className="container">
-      <form className="form" onSubmit={handleSubmit}>
-        <label>
-          Username:{" "}
-          <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleInputChange}
-          />
-        </label>
-        <label>
-          First Name:{" "}
-          <input
-            type="text"
-            name="firstName"
-            value={formData.firstName}
-            onChange={handleInputChange}
-          />
-        </label>
-        <label>
-          Last Name:{" "}
-          <input
-            type="text"
-            name="lastName"
-            value={formData.lastName}
-            onChange={handleInputChange}
-          />
-        </label>
-        <label>
-          Email:{" "}
-          <input
-            type="text"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-          />
-        </label>
-        <label>
-          Password:{" "}
-          <input
-            type="text"
-            name="password"
-            value={formData.password}
-            onChange={handleInputChange}
-          />
-        </label>
-        <p style={{ fontSize: "12px" }}>
-          NOTE: Make this an uncommon password with characters, numbers, and
-          special characters!
-        </p>
-        <button>Register!</button>
-      </form>
+    <div className="register-page">
+      <div className="container">
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form-group row">
+            <label className="col-sm-2 col-form-label">
+              Username:{" "}
+              <div className="col-sm-10">
+                <input
+                  className="input"
+                  type="text"
+                  name="username"
+                  value={formData.username}
+                  onChange={handleInputChange}
+                />
+              </div>
+            </label>
+          </div>
+          <div className="form-group row">
+            <label className="col-sm-2 col-form-label">
+              First Name:{" "}
+              <div className="col-sm-10">
+                <input
+                  className="input"
+                  type="text"
+                  name="firstName"
+                  value={formData.firstName}
+                  onChange={handleInputChange}
+                />
+              </div>
+            </label>
+          </div>
+          <div className="form-group row">
+            <label className="col-sm-2 col-form-label">
+              Last Name:{" "}
+              <div className="col-sm-10">
+                <input
+                  className="input"
+                  type="text"
+                  name="lastName"
+                  value={formData.lastName}
+                  onChange={handleInputChange}
+                />
+              </div>
+            </label>
+          </div>
+          <div className="form-group row">
+            <label className="col-sm-2 col-form-label">
+              Email:{" "}
+              <div className="col-sm-10">
+                <input
+                  className="input"
+                  type="text"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                />
+              </div>
+            </label>
+          </div>
+          <div className="form-group row">
+            <label className="col-sm-2 col-form-label">
+              Password:{" "}
+              <div className="col-sm-10">
+                <input
+                  className="input"
+                  type="text"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                />
+              </div>
+            </label>
+          </div>
+          <div className="register-text">
+            <p style={{ fontSize: "12px" }}>
+              NOTE: Make this an uncommon password with characters, numbers, and
+              special characters!
+            </p>
+          </div>
+          <button className="login-button">Register!</button>
+        </form>
+      </div>
     </div>
   );
 };
