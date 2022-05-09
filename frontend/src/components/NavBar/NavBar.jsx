b5d429e9a048b4a12fdcccc08e88df31b8eff412
tjdanielson <tessajdanielson@gmail.com>
2022-05-09 16:04:29 -0500

    Added a dropdown/hamburger menu when a screen is small/mobile sized.

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -4,34 +4,29 @@ import { useNavigate, Link } from "react-router-dom";
 import AuthContext from "../../context/AuthContext";
 import LogCleanup from "../LogCleanup/LogCleanup";
 import "./NavBar.css";
+import Dropdown from "react-bootstrap/Dropdown";
 
 const Navbar = () => {
   const { logoutUser, user } = useContext(AuthContext);
   const navigate = useNavigate();
-  return (
-    <div className="navBar">
-      <ul className="listone">
-        <li className="navlinks">
-          <Link to="/">
-            <b>Home</b>
-          </Link>
-        </li>
-        <li className="navlinks">
-          {user ? (
-            <Link className="navlinks" to="profile/">
-              User Dashboard
-            </Link>
-          ) : (
-            <a></a>
-          )}
-        </li>
-        <li className="navlinks">
-          {user ? <Link to="commdash/">Community Dashboard</Link> : <a></a>}
-        </li>
-        <li className="navlinks">{user ? <LogCleanup /> : <a></a>}</li>
-      </ul>
-      <ul className="listtwo">
-        <li className="navlinks">
+
+  let mediaQueryCondition = window.matchMedia("( max-width: 650px )");
+
+  if (mediaQueryCondition.matches) {
+    return (
+      <div className="navBar">
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            Menu
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item href="/">Home</Dropdown.Item>
+            <Dropdown.Item href="/profile/">User Dashboard</Dropdown.Item>
+            <Dropdown.Item href="/commdash/">Community Dashboard</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        <div className="navlinks">{user ? <LogCleanup /> : <a></a>}</div>
+        <div className="navlinks">
           {user ? (
             <button onClick={logoutUser}>Logout</button>
           ) : (
@@ -39,10 +34,46 @@ const Navbar = () => {
               Login
             </button>
           )}
-        </li>
-      </ul>
-    </div>
-  );
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="navBar">
+        <ul className="listone">
+          <li className="navlinks">
+            <Link to="/">
+              <b>Home</b>
+            </Link>
+          </li>
+          <li className="navlinks">
+            {user ? (
+              <Link className="navlinks" to="profile/">
+                User Dashboard
+              </Link>
+            ) : (
+              <a></a>
+            )}
+          </li>
+          <li className="navlinks">
+            {user ? <Link to="commdash/">Community Dashboard</Link> : <a></a>}
+          </li>
+          <li className="navlinks">{user ? <LogCleanup /> : <a></a>}</li>
+        </ul>
+        <ul className="listtwo">
+          <li className="navlinks">
+            {user ? (
+              <button onClick={logoutUser}>Logout</button>
+            ) : (
+              <button className="login" onClick={() => navigate("/login")}>
+                Login
+              </button>
+            )}
+          </li>
+        </ul>
+      </div>
+    );
+  }
 };
 
 export default Navbar;
