3c5b22c52b22378a8d036a1dda52d360017251a6
tjdanielson <tessajdanielson@gmail.com>
2022-05-10 12:15:00 -0500

    Added background image on homepage.

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -6,6 +6,12 @@ import "./HomePage.css";
 const HomePage = () => {
   return (
     <div className="homepage">
+      <div className="banner">
+        {/* <img
+          src="../assets/pexels-lara-jameson-9324307.jpg"
+          alt="man picking up trash with skyline in background."
+        /> */}
+      </div>
       <h1>Welcome</h1>
       <h1>About Us</h1>
     </div>
