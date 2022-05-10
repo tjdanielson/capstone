e22f7f356573452615159ad864da7654f5552b77
tjdanielson <tessajdanielson@gmail.com>
2022-05-09 22:09:08 -0500

    Added filter functionality for managing cleanups - admin can search by user name to find cleanups for that user.

--- frontend/src/components/ManageCleanups/ManageCleanups.jsx
+++ frontend/src/components/ManageCleanups/ManageCleanups.jsx
@@ -1,33 +1,60 @@
+import React, { useState } from "react";
+
 const ManageCleanups = (props) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const searchUsers = (event) => {
+    event.preventDefault();
+    let filteredCleanups = props.cleanups.filter((cleanup, i) => {
+      if (cleanup.user.username == searchTerm) {
+        return cleanup;
+      }
+    });
+    setSearchResults(filteredCleanups);
+  };
+
   return (
     <div>
       <h3>Manage Cleanups</h3>
-      <table className="my-table">
-        <thead>
-          <tr>
-            <th>Date Logged</th>
-            <th>Time Spent</th>
-            <th>Date of Cleanup</th>
-            <th>User</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody className="table-body">
-          {props.cleanups.map((cleanup, i) => {
-            return (
-              <tr key={i}>
-                <td>{cleanup.date_submitted}</td>
-                <td>{cleanup.time_spent}</td>
-                <td>{cleanup.date_cleanup}</td>
-                <td>{cleanup.user.username}</td>
-                {/* <td>
+      <div className="search">
+        <form onSubmit={searchUsers}>
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={(event) => setSearchTerm(event.target.value)}
+          />
+          <button type="submit">Filter</button>
+        </form>
+      </div>
+      {searchResults && (
+        <table className="my-table">
+          <thead>
+            <tr>
+              <th>Date Logged</th>
+              <th>Time Spent</th>
+              <th>Date of Cleanup</th>
+              <th>User</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody className="table-body">
+            {searchResults.map((cleanup, i) => {
+              return (
+                <tr key={i}>
+                  <td>{cleanup.date_submitted}</td>
+                  <td>{cleanup.time_spent}</td>
+                  <td>{cleanup.date_cleanup}</td>
+                  <td>{cleanup.user.username}</td>
+                  {/* <td>
                     <DeleteCleanup cleanupId={cleanup.id} />
                   </td> */}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
